1508c6cd9f75919d79ecfd0dec4de83919e451d5
MIVD <mi.vandeurzen@gmail.com>
2018-10-02 13:20:17 +0200

    started on tickets frontend

---
+++ src/components/tickets/TicketForm.js
@@ -0,0 +1,23 @@
+import * as React from 'react'
+
+export default function TicketForm(props) {
+  return (
+    <div>
+    <h1>Sell Your Ticket</h1>
+    <form onSubmit={props.onSubmit} onChange={props.onChange}>
+      <label>
+        Description:
+        <input type="text" name="description" values={props.description} />
+      </label>
+      <label>
+        Price:
+        <input type="text" name="price" values={props.price} />
+      </label>
+      <label>
+        Image(URL):
+        <input type="text" name="thumbnail" values={props.thumbnail} />
+      </label>
+      <button type="submit">Add</button>
+  </form>
+  </div>)
+}
---
+++ src/components/tickets/TicketFormContainer.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import {connect} from 'react-redux'
+import {createTicket} from '../../actions/tickets'
+import TicketForm from './TicketForm'
+
+class TicketFormContainer extends React.PureComponent {
+  state = {
+    description: '',
+    price: '',
+    thumbnail: ''
+  }
+
+  onChange = (ticket) => {
+    this.setState({
+      [ticket.target.name]: ticket.target.value
+    })
+  }
+
+  onSubmit = (ticket) => {
+    ticket.preventDefault()
+    this.setState({
+      description: '',
+      price: '',
+      thumbnail: ''
+    })
+    this.props.createTicket(this.state)
+  }
+
+  render() {
+    return (
+      <TicketForm
+      onSubmit={this.onSubmit}
+      onChange={this.onChange}
+      values={this.state}
+    />)
+  }
+}
+
+export default connect(null, {createTicket})(TicketFormContainer)
