8623a7dc3b58e65870d291b55e2549c10976095d
MIVD <mi.vandeurzen@gmail.com>
2018-10-02 15:40:05 +0200

    GET ticket per event

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import {Provider} from 'react-redux'
 import { Route } from 'react-router-dom'
 import EventDetailsContainer from './components/events/EventDetailsContainer'
 import EventsListContainer from './components/events/EventsListContainer'
-import TicketListContainer from './components/tickets/TicketListContainer'
+// import TicketListContainer from './components/tickets/TicketListContainer'
 
 class App extends Component {
   render() {
@@ -13,7 +13,6 @@ class App extends Component {
         <div>
           <Route path="/" exact component={EventsListContainer} />
           <Route path="/events/:id" component={EventDetailsContainer} />
-          <Route path="/events/:id/tickets" component={TicketListContainer} />
         </div>
       </Provider>
     );
